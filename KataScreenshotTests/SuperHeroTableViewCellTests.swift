2a2549d3a9265d6e5d19e37f3da55c00a42b9b28
Jordi Serra i Font <jordi.serra.font@gmail.com>
2017-03-25 17:23:14 +0100

    tests for cells

--- KataScreenshotTests/SuperHeroTableViewCellTests.swift
+++ KataScreenshotTests/SuperHeroTableViewCellTests.swift
@@ -11,6 +11,30 @@ import XCTest
 @testable import KataScreenshot
 
 class SuperHeroTableViewCellTests: ScreenshotTest {
+    
+    func testShowsAnySuperHero() {
+        let cell = givenASuperHeroTableViewCell()
+        
+        cell.configure(forItem: SuperHero(name: "Test Super Hero", photo: URL(string: ""), isAvenger: false, description: ""))
+        
+        verify(view: cell)
+    }
+    
+    func testShowsAnyAvenger() {
+        let cell = givenASuperHeroTableViewCell()
+        
+        cell.configure(forItem: SuperHero(name: "Test Avenger", photo: URL(string: ""), isAvenger: true, description: ""))
+        
+        verify(view: cell)
+    }
+    
+    func testShowsAnySuperHeroWithALongName() {
+        let cell = givenASuperHeroTableViewCell()
+        
+        cell.configure(forItem: SuperHero(name: "Test Super Hero With a very long name and indeed it is very long this name", photo: URL(string: ""), isAvenger: false, description: ""))
+        
+        verify(view: cell)
+    }
 
     private func givenASuperHeroTableViewCell() -> SuperHeroTableViewCell {
         return UINib(nibName: "SuperHeroTableViewCell", bundle: Bundle.main).instantiate(withOwner: self, options: nil)[0] as! SuperHeroTableViewCell
