d632e183033716daceafc7862763c5f07695c8a4
Jordi Serra i Font <jordi.serra.font@gmail.com>
2017-03-25 17:53:40 +0100

    detail view controller tests

--- KataScreenshotTests/SuperHeroDetailViewControllerTests.swift
+++ KataScreenshotTests/SuperHeroDetailViewControllerTests.swift
@@ -28,9 +28,42 @@ class SuperHeroDetailViewControllerTests: ScreenshotTest {
 
         verify(viewController: viewController)
     }
+    
+    func testShowsSuperHeroWithoutName() {
+        let superHero = SuperHeroMother.givenASuperHero(name: "")
+        repository.superHeroes = [superHero]
+        let viewController = getSuperHeroDetailViewController(superHero.name)
+        verify(viewController: viewController)
+    }
+    
+    func testShowsSuperHeroWithoutDescription() {
+        let superHero = SuperHeroMother.givenASuperHero(description: "")
+        repository.superHeroes = [superHero]
+        let viewController = getSuperHeroDetailViewController(superHero.name)
+        verify(viewController: viewController)
+    }
+    
+    func testShowsSuperHeroWithLongName() {
+        let superHero = SuperHeroMother.givenASuperHero(name: "asdlfkjasdlkfjaslñdkfjaslñdkfjasdlfñkjasdflñkjasdfñlkjasdfñlkj")
+        repository.superHeroes = [superHero]
+        let viewController = getSuperHeroDetailViewController(superHero.name)
+        verify(viewController: viewController)
+    }
+    
+    func testShowsSuperHeroWithLongDescription() {
+        let superHero = givenASuperHeroWithLongDescription()
+        let viewController = getSuperHeroDetailViewController(superHero.name)
+        verify(viewController: viewController)
+    }
 
     func givenASuperHero(isAvenger: Bool) -> SuperHero {
         let superHero = SuperHeroMother.givenASuperHero(isAvenger: isAvenger)
+        repository.superHeroes = [superHero]
+        return superHero
+    }
+    
+    func givenASuperHeroWithLongDescription() -> SuperHero {
+        let superHero = SuperHeroMother.givenASuperHero(description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
         repository.superHeroes = [superHero]
         return superHero
     }
